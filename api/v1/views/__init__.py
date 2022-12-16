9743bc6ffb3f701e9a2d9d8c10cfefda02a419b8
VCruz910 <vscruz910@gmail.com>
2022-12-16 17:51:31 -0400

    Fixed Repository

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-"""
-Init file.
-"""
+""" Init file """
+from api.v1.views.places_amenities import *
+from api.v1.views.places_reviews import *
 from api.v1.views.places import *
 from api.v1.views.users import *
 from api.v1.views.amenities import *
